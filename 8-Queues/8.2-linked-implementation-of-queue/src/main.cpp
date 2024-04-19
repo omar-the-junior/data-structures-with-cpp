cb24e375cf807af1d4439314f33763e2dda8e673
omar-the-junior <omarmasoudonline@gmail.com>
2024-04-19 08:23:29 +0000

    Apply linked implementation of queue

---
+++ 8-Queues/8.2-linked-implementation-of-queue/src/main.cpp
@@ -0,0 +1,35 @@
+#include "../include/linkedQueueType.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    linkedQueueType<int> queue;
+    int x, y;
+
+    queue.initializeQueue();
+    x = 4;
+    y = 5;
+    queue.addQueue(x);
+    queue.addQueue(y);
+    x = queue.front();
+    queue.deleteQueue();
+    queue.addQueue(x + 5);
+    queue.addQueue(16);
+    queue.addQueue(x);
+    queue.addQueue(y - 3);
+
+    cout << "Queue Elements: ";
+
+    while (!queue.isEmptyQueue())
+    {
+        cout << queue.front() << " ";
+        queue.deleteQueue();
+    }
+
+    cout << endl;
+
+    return 0;
+}
